81d57ca77c502d91cff92744b3f32bd82054e49f
Luis Castillo <luigiernest999@gmail.com>
2022-06-21 09:31:04 -0600

    Actualizando backend

--- backend/src/controllers/customerUser.js
+++ backend/src/controllers/customerUser.js
@@ -4,16 +4,6 @@ const controller = {}
 controller.test = (req,res) => {
     res.send('get routes')
 }
-controller.query = (req,res) =>{
-    const {id} =req.params
-    let sql ='select  from USER where id_user=?'
-    conexion.query(sql,(err,rows,fields) =>{
-        if(err) throw err;
-        else{
-            res.json(rows)
-        }
-    })
-}
 
 controller.getUser = (req,res) =>{
     const {id} =req.params
