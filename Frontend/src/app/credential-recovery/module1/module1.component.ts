e12e900de4465cf2d8a49126266b2d375a555509
Delmerespinal24 <Delmerespinal2@gmail.com>
2022-06-29 21:16:42 -0600

    Validar email modulo1 v1

--- Frontend/src/app/credential-recovery/module1/module1.component.ts
+++ Frontend/src/app/credential-recovery/module1/module1.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
+import { Registro, EquipoService } from 'src/app/SERVICES/equipo.service';
 
 @Component({
   selector: 'app-module1',
@@ -6,6 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./module1.component.css']
 })
 export class Module1Component implements OnInit {
+
+  module1Form = new FormGroup({
+    email: new FormControl('', [Validators.email,Validators.required] )
+  })
+
+  get emailControl():FormControl{
+    return this.module1Form.get('email') as FormControl
+  }
+
+  modulo1: Registro={
+    fk_id_department:1,
+    var_email:"",
+    var_name:"",
+    var_lastname:"",
+    tex_password:"",
+    bit_rol:1,
+    bit_status:1,
+    var_phone:""
+  }
 
   constructor() { }
 
