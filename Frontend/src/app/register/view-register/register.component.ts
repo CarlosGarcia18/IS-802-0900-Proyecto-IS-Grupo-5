19dad3845235f936f28ca09411f4c7efdcfc68c0
Delmerespinal24 <Delmerespinal2@gmail.com>
2022-07-01 11:49:29 -0600

    Confirmar contraseña

--- Frontend/src/app/register/view-register/register.component.ts
+++ Frontend/src/app/register/view-register/register.component.ts
@@ -3,6 +3,8 @@ import { Registro, EquipoService } from 'src/app/SERVICES/equipo.service';
 //import { CargarScriptsService } from './../.././cargar-scripts.service';
 import { Router} from '@angular/router';
 import { FormControl, FormGroup, Validators} from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -11,16 +13,30 @@ import { FormControl, FormGroup, Validators} from '@angular/forms';
 export class RegisterComponent implements OnInit {
   regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&-/_|])([A-Za-z\d$@$!%*?&]|[^ ]){7,40}$/;
   
+  MatchValidator(source: string, target: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const sourceCtrl = control.get(source);
+      const targetCtrl = control.get(target);
+
+      return sourceCtrl && targetCtrl && sourceCtrl.value !== targetCtrl.value
+        ? { mismatch: true }
+        : null;
+    };
+  }
+
   loginForm=new FormGroup({
     nombre: new FormControl('',[Validators.required, Validators.minLength(2) ]),
     apellido: new FormControl('', [Validators.minLength(2),Validators.required]),
     email: new FormControl('', [Validators.email,Validators.required] ),
     password: new FormControl('', [Validators.required, Validators.pattern(this.regex)]), //mayusculas, minusculas,num caracter, minimo 6
-    confirmPassword: new FormControl('', [Validators.minLength(7),Validators.required]),
+    confirmPassword: new FormControl('', [Validators.minLength(7),Validators.required],),
     telefono: new FormControl('',[Validators.required, Validators.pattern('[0-9]{8}')]),
     ubicacion: new FormControl('', Validators.required),
     check:new FormControl(true, Validators.required)
-  });
+  },
+  [this.MatchValidator('password','confirmPassword')]
+  );
+
 
   get nombreControl():FormControl{
     return this.loginForm.get('nombre') as FormControl
@@ -34,6 +50,7 @@ export class RegisterComponent implements OnInit {
   get passControl():FormControl{
     return this.loginForm.get('password') as FormControl
   }
+  
   get passValidateControl():FormControl{
     return this.loginForm.get('confirmPassword') as FormControl
   }
@@ -48,6 +65,13 @@ export class RegisterComponent implements OnInit {
     return this.loginForm.get('check') as FormControl
   }
 
+  get passwordMatchError() {
+    return (
+      this.loginForm.getError('mismatch') &&
+      this.loginForm.get('confirmPassword')?.touched
+    );
+  }
+
   registro: Registro={
   fk_id_department:1,
   var_email:"",
@@ -58,9 +82,6 @@ export class RegisterComponent implements OnInit {
   bit_status:1,
   var_phone:""
   }
-  match(firstControlName:any, secondControlName:any){
-    return !(this.loginForm.get(firstControlName)?.value === this.loginForm.get(secondControlName)?.value); 
-  };
 
   constructor(//private _CargaScripts:CargarScriptsService,
               private EquipoService:EquipoService, private router: Router)            
