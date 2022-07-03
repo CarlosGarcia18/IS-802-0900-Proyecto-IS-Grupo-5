623fdee84a4b0208748037eabd53ec3ffbadef21
Delmerespinal24 <Delmerespinal2@gmail.com>
2022-07-03 11:37:33 -0600

    actualizacion

--- Frontend/src/app/credential-recovery/module1/module1.component.ts
+++ Frontend/src/app/credential-recovery/module1/module1.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators} from '@angular/forms';
 import { emailCredential, EquipoService, status } from "../../SERVICES/equipo.service";
+import { Router, ActivatedRoute} from '@angular/router';
 
 
 @Component({
@@ -10,7 +11,7 @@ import { emailCredential, EquipoService, status } from "../../SERVICES/equipo.se
 })
 export class Module1Component implements OnInit {
 
-  constructor(private EquipoService:EquipoService) { }
+  constructor(private EquipoService:EquipoService, private router: Router) { }
 
   module1Form = new FormGroup({
     email: new FormControl('', [Validators.email,Validators.required] )
@@ -32,6 +33,8 @@ export class Module1Component implements OnInit {
 
       let result = this.EquipoService.emailModule1(this.module1).subscribe(
         res => {
+
+          this.router.navigate(['module2/' + this.module1.var_email]);
           /*
           console.log(this.module1.bit_status)
           if(!this.module1.bit_status){
@@ -42,12 +45,13 @@ export class Module1Component implements OnInit {
         err => console.log(err)
       );
 
-    
+  }
+
 
 
       /*console.log(result) */
 
-  }
+  
 
   ngOnInit(): void {
   }
