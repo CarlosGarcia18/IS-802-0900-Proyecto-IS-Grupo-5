1f7e143ccc77ca14a39a58ab3e94610c259c49e0
DBarahonat <86747580+DBarahonat@users.noreply.github.com>
2022-07-03 01:46:25 -0600

    actualizacion
    actualizacion del modelo 2

--- Frontend/src/app/credential-recovery/module2/module2.component.ts
+++ Frontend/src/app/credential-recovery/module2/module2.component.ts
@@ -7,6 +7,7 @@ import { codigo, EquipoService,status } from "../../SERVICES/equipo.service";
   selector: 'app-module2',
   templateUrl: './module2.component.html',
   styleUrls: ['./module2.component.css']
+  
 })
 export class Module2Component implements OnInit {
 
@@ -15,12 +16,14 @@ export class Module2Component implements OnInit {
   expresiones = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&-/_|])([A-Za-z\d$@$!%*?&]|[^ ]){7,40}$/;
 
 
-  
+  /*Variable */
   module2Form:FormGroup;
 
+  /*El get */
   get codigoVerificacion():FormControl{
     return this.module2Form.get('codigo') as FormControl
   }
+  validate =false;
 
  
   modulo2: codigo={
@@ -37,15 +40,49 @@ export class Module2Component implements OnInit {
    }
 
   ngOnInit(): void {
-    this.veriCodigo.var_codigo=this.rutaActiva.snapshot.params['codigo']
+    this.veriCodigo.var_codigo=this.rutaActiva.snapshot.params['email']
+    
+   
+    
 
   }
-
+/*Variable  */
   veriCodigo:codigo={
     var_codigo:""
   }
 
   obtenerCodigo(){
+    console.log('funciona')
+
+    if(this.veriCodigo.var_codigo){
+      var status:any =[];
+      let result=this.EquipoService.addCodigo(this.veriCodigo).subscribe(
+        res =>{
+          
+          if(status.status=='200'){
+            this.router.navigate(['../../Module3/email'])
+          }else{
+            this.validate=true
+            
+          }
+        },
+        err => console.log(err)
+      );
+    }
+    /*const Ncodigo:codigo ={
+      var_codigo:this.module2Form.get('codigo')
+    }
+    */
+  /* this.EquipoService.addCodigo(/*Ncodigo*).subscribe(data =>{
+    /*this.toastr.success('El codigo de verificacion fue registrado con exito')*
+    this.router.navigate(['/']);
+   }, error=>{
+    console.log(error);
+    this.module2Form.reset();
+
+   })*/
+
+    
   
   }
  
