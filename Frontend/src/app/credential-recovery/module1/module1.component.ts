12dfc1fbba21f10c0984ad0d69de9750c6b63889
Delmerespinal24 <Delmerespinal2@gmail.com>
2022-07-03 23:19:50 -0600

    Modulo 1 validaciones

--- Frontend/src/app/credential-recovery/module1/module1.component.ts
+++ Frontend/src/app/credential-recovery/module1/module1.component.ts
@@ -3,7 +3,6 @@ import { FormControl, FormGroup, Validators} from '@angular/forms';
 import { emailCredential, EquipoService, status } from "../../SERVICES/equipo.service";
 import { Router, ActivatedRoute} from '@angular/router';
 
-
 @Component({
   selector: 'app-module1',
   templateUrl: './module1.component.html',
@@ -27,19 +26,33 @@ export class Module1Component implements OnInit {
   }
 
   deleteUser:boolean = false
+  incorrect:boolean = false
+  error:boolean = false
 
   sendEmail(){
+    this.deleteUser = false
+    this.incorrect = false
+    this.error = false
 
+    var status:any = [];
 
       let result = this.EquipoService.emailModule1(this.module1).subscribe(
         res => {
+          var status:BookInfo = <any>res
 
-          this.router.navigate(['module2/' + this.module1.var_email]);
-          /*
-          console.log(this.module1.bit_status)
-          if(!this.module1.bit_status){
-            this.deleteUser = true;
-          }*/
+          if(status.status == "200"){ // Correo correcto
+
+            this.router.navigate(['module2/' + this.module1.var_email]);
+            
+          }else if(status.status == '1'){ // Correo invalido
+            this.incorrect = true
+
+          }else if(status.status == '2'){ // Usuario dado de baja
+            this.deleteUser = true
+
+          }else{
+            this.error = true
+          }
       
         },
         err => console.log(err)
@@ -56,4 +69,8 @@ export class Module1Component implements OnInit {
   ngOnInit(): void {
   }
 
+}
+
+interface BookInfo {
+  status : string ;
 }
