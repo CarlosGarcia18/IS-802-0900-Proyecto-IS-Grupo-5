4c5197b2966e87f05d8b053d23612151ebc607c1
larissa-cmyk <cantareronicole@gmail.com>
2022-07-08 23:48:28 -0600

    ..

--- Frontend/src/app/navigation-products/published-products/published-products.component.ts
+++ Frontend/src/app/navigation-products/published-products/published-products.component.ts
@@ -1,7 +1,9 @@
+
 import { Component, OnInit } from '@angular/core';
-import { EquipoService } from 'src/app/SERVICES/equipo.service';
-
-
+import { filter, EquipoService } from 'src/app/SERVICES/equipo.service';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
+import { Router} from '@angular/router';
+import { PageEvent } from '@angular/material/paginator';
 @Component({
   selector: 'app-published-products',
   templateUrl: './published-products.component.html',
@@ -23,5 +25,42 @@ export class PublishedProductsComponent implements OnInit {
     )
 
   }
+
+
+
+
+  
+
+  pageSize=6;
+  desde:number= 0;
+  hasta:number=6;
+  items :any[]=[{nombre: 'Producto 1',  precio: 'L.70'},
+              {nombre: 'Producto 2', precio: 'L.90'},
+              {nombre: 'Producto 3', precio: 'L.80'},
+              {nombre: 'Producto 4', precio: 'L.220'},
+              {nombre: 'Producto 5', precio: 'L.9230'},
+              {nombre: 'Producto 6', precio: 'L.900'},
+              {nombre: 'Producto 7', precio: 'L.19.00'},
+              {nombre: 'Producto 8', precio: 'L.980'}];
+
+  cambiarPagina(e:PageEvent){
+    console.log(e)
+    this.desde=e.pageIndex*e.pageSize;
+    this.hasta=this.desde+e.pageSize;
+  }
+
+  filtro: filter={
+    fk_id_department:0,
+    dou_price:0,
+    fk_id_product_category:0
+    }
+
+
+
+
+
+
+
+
 
 }
