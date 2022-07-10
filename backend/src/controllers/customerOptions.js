0bcc07d0eb521c5b0343423c0f2c92e2e706a48b
Delmerespinal24 <Delmerespinal2@gmail.com>
2022-07-09 19:59:02 -0600

    subir imagen

--- backend/src/controllers/customerOptions.js
+++ backend/src/controllers/customerOptions.js
@@ -3,6 +3,41 @@ const controller = {} //definicion de controller que guardara las rutas
 const fs= require('fs')
 const path = require('path')
 
+const multer = require('multer')
+
+const diskstorage = multer.diskStorage({
+    destination: path.join(__dirname, './images'),
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-plazitanet-' + file.originalname)
+    }
+})
+
+const fileUpload = multer({
+    storage: diskstorage
+}).single('image')
+
+controller.postImage = (req,res) =>{
+    req.getConnection((err, conn) => {
+        if(err) return res.status(500).send('server error')
+
+        const type = req.file.mimetype
+        const name = req.file.originalname
+        const extension = req.file.type
+        const data = fs.readFileSync(path.join(__dirname, '../images/' + req.file.filename))
+
+        let sql= `INSERT INTO photographs(blob_file, var_name, var_extension, fk_id_product)
+            VALUES(${data},${name},${extension},1)`
+
+        conection.query(sql, (err, rows) => {
+            if(err) return res.status(500).send('server error')
+
+            res.send('200')
+        })
+    })
+
+
+}
+
 //funcion de prueba
 controller.test = (req,res) => {
     res.send('get routes')
@@ -18,6 +53,7 @@ controller.getDepartament = (req,res) =>{
         }
     })
 }
+
 //funcion para insertar un producto /// Sin las imagenes
 controller.postProduct = (req,res) =>{
     const {fk_id_user, fk_id_department, fk_id_product_category, fk_id_product_status, var_name, text_description, dou_price, bit_availability} = req.body
--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,9 +1,22 @@
 const routers = require('express').Router()//Direccionamiento en express
 
-
 const customerU = require('../controllers/customerUser')//funciones de llamada por parte del usuario
 const customerA = require('../controllers/customerAdmin')//funciones de llamada por parte del administrador
 const customerO = require('../controllers/customerOptions')//funciones de llamada por parte del administrador
+
+const path = require('path')
+const multer = require('multer')
+
+const diskstorage = multer.diskStorage({
+    destination: path.join(__dirname, '../images'),
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-plazitanet-' + file.originalname)
+    }
+})
+
+const fileUpload = multer({
+    storage: diskstorage
+}).single('image')
 
 
 //Direcciones de prueba
@@ -46,6 +59,9 @@ routers.post('/productFiltering',customerO.productFiltering)
 
 //Agregar un producto
 routers.post('/newProduct',customerO.postProduct)
+
+//Subir imagenes
+routers.post('/product/postImage', fileUpload, customerO.postImage)
 
 //Eliminar un producto dado un id // Elimina todas las imagenes del producto
 routers.delete('/product/delete/:id',customerO.deleteProduct)
