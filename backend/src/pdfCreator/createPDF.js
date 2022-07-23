72f90b3c0efbd3a0ecbd49b2efe0f5d7095d1913
Luis Castillo <luigiernest999@gmail.com>
2022-07-22 22:32:25 -0600

    Creacion de pdf

--- backend/src/pdfCreator/createPDF.js
+++ backend/src/pdfCreator/createPDF.js
@@ -1,35 +1,18 @@
-var PDFDocument = require('pdfkit');
-var fs = require('fs');
 const path = require('path')
-const pdf = require('./html-pdf')
 
-function generateStruct(doc) {
-  // Initialise document logical structure
-  let struct = doc.struct('Document');
-  
-
-  return struct
+function information(doc) {
+  // Metadatos del archivo PDF
+  doc.info['Title'] = 'Productos Recomendados';
+  doc.info['Author'] = 'Plazitanet';
 }
 
-function generate(id) {
-  // Create a new PDFDocument
-  let doc = new PDFDocument({
-    pdfVersion: '1.5',
-    lang: 'en-US',
-    tagged: true,
-    displayTitle: true
-  });
-  let r = id + '_suscription.pdf'
-  let p = './src/documentPDF/' + r
-  pdf.pdfCreate(r)
-  console.log(fs.createWriteStream(p))
-  doc.pipe(fs.createWriteStream(p));
-
-  // Set some meta data
-  doc.info['Title'] = 'Test Document';
-  doc.info['Author'] = 'Devon Govett';
-
-  return doc
+function fonts(doc) {
+  doc.registerFont('Nunito-Light', path.join(__dirname,'./fonts/Nunito-Light.ttf'));
+  doc.registerFont('Nunito-Bold', path.join(__dirname,'./fonts/Nunito-Bold.ttf'));
+  doc.registerFont('OpenSans-Light', path.join(__dirname,'./fonts/OpenSans-Light.ttf'));
+  doc.registerFont('OpenSans-Bold', path.join(__dirname,'./fonts/OpenSans-Bold.ttf'));
+  doc.registerFont('Poppins-Light', path.join(__dirname,'./fonts/Poppins-Light.ttf'));
+  doc.registerFont('Poppins-Regular', path.join(__dirname,'./fonts/Poppins-Regular.ttf'));
 }
 
 function head(y,doc) {
@@ -37,35 +20,53 @@ function head(y,doc) {
       grad.stop(0, '#74ebd5')
           .stop(1, '#9face6');
       doc
-       // .font('Palatino')
-        .font('Helvetica-Bold')
+        .font('Nunito-Bold')
         .fontSize(22)
         .fill(grad)
         .text('Da un vistazo a tus productos recomendados', 50, y,{align: 'center'});
-    
-
 }
 
+//nueva categoria
 function newCat(y,doc,cat) {
-      doc
-        .font('Helvetica')
-        .fontSize(18)
-        .fill('#fbac38')
-        .text(cat, 50, y,{align: 'center'});
-    
+    doc
+      .font('OpenSans-Bold')
+      .fontSize(18)
+      .fill('#fbac38')
+      .text(cat, 50, y,{align: 'center'});
 }
+/*doc
+.font('OpenSans-Bold')
+.fontSize(18)
+.fill('#fbac38')
+.text(cat.var_name, 50, y, { align: 'center' });*/
+
 
 function products(obj,y,doc) {
         doc.image(path.join(__dirname,'../dbimagesProducts/'+obj.var_name) ,50,y,{
           fit: [80, 80], align: 'center', valign: 'center'
         })
-        .font('Times-Roman')
+        .font('Poppins-Light')
         .fontSize(15).fillColor('black').text(obj.var_name_product,140,y)
         .fontSize(10).text(obj.text_description)
-        .font('Times-Bold').fontSize(13).fillColor('red').text(obj.dou_price,{align: 'right'})
+        .font('Times-Bold').fontSize(13).fillColor('red').text("L."+obj.dou_price,{align: 'right'})
 }
+ /*doc.image(path.join(__dirname,'../dbimagesProducts/'+rows1[j].var_name) ,50,y,{
+  fit: [80, 80], align: 'center', valign: 'center'
+})
+.font('Poppins-Light')
+.fontSize(15).fillColor('black').text(rows1[j].var_name_product,140,y+10)
+.fontSize(10).text(rows1[j].text_description)
+.font('Times-Bold').fontSize(13).fillColor('red').text(rows1[j].dou_price,{align: 'right'})
+*/
 
 
-module.exports = {generate,head,newCat,products,generateStruct};
+module.exports = 
+{
+  information,
+  head,
+  newCat,
+  products,
+  fonts
+};
 
 
--- backend/src/pdfCreator/generate.js
+++ backend/src/pdfCreator/generate.js
@@ -3,28 +3,20 @@ const conection = require('../config/connection')
 var PDFDocument = require('pdfkit');
 var fs = require('fs');
 const path = require('path')
+const SQLconsult = require('./SQLconsults')
 
 create1()
 
 function create1() {
-    let sql2 = "SELECT * FROM user"
-    conection.query(sql2, (err2, rows2, fields) => {
+    conection.query(SQLconsult.sqlGetUsers, (err2, rows2, fields) => {
         if (err2) console.log({ status: '0', msf: err.sqlMessage });
         else {
             for (let i = 0; i < rows2.length; i++) {
-                let sql = "SELECT fk_id_user,fk_id_product_category,var_name FROM subscription,product_category"
-                    + " where subscription.fk_id_product_category = product_category.id_product_category"
-                    + ` AND fk_id_user=${rows2[i].id_user}`
-                conection.query(sql, (err, rows, fields) => {
+                conection.query(SQLconsult.sqlSuscriptions(rows2[i].id_user), (err, rows, fields) => {
                     if (err) console.log({ status: '1', msf: err.sqlMessage });
                     else {
                         if (rows.length > 0) {
-                            let sql1 = "SELECT fk_id_product_category,product.var_name AS var_name_product,text_description,"
-                                            + "dou_price,photographs.var_name FROM product LEFT OUTER JOIN  photographs ON "
-                                            + "photographs.fk_id_product=product.id_product WHERE fk_id_product_status = 1 AND "
-                                            + `bit_availability = 1 GROUP BY product.id_product `
-                                            + "ORDER BY int_views DESC"
-                            conection.query(sql1, (err1, rows1, fields) => {
+                            conection.query(SQLconsult.sqlGetProducts, (err1, rows1, fields) => {
                                 if (err1){
                                     console.log({ status: '2', msf: err1.sqlMessage }); 
                                 } 
@@ -35,47 +27,23 @@ function create1() {
                                         tagged: true,
                                         displayTitle: true
                                     });
-                                    let r = rows2[i].id_user + '_suscription.pdf'
-                                    let p = './src/documentPDF/' + r
-                                    //pdf.pdfCreate(r)
-                                    //console.log(fs.createWriteStream(p))
+                                    let p = './src/documentPDF/' + rows2[i].id_user + '_suscription.pdf'
                                     doc.pipe(fs.createWriteStream(p));
         
-                                    // Set some meta data
-                                    doc.info['Title'] = 'Test Document';
-                                    doc.info['Author'] = 'Devon Govett';
+                                    gen.information(doc)//genera la informacion
+                                    gen.fonts(doc)//genera las fuentes
                                     var y = 60
-                                    let grad = doc.linearGradient(0, 0, 100, 0);
-                                    grad.stop(0, '#74ebd5')
-                                        .stop(1, '#9face6');
-                                    doc
-                                        // .font('Palatino')
-                                        .font('Helvetica-Bold')
-                                        .fontSize(22)
-                                        .fill(grad)
-                                        .text('Da un vistazo a tus productos recomendados', 50, y, { align: 'center' });
+                                    gen.head(y,doc)//Creacion del encabezado
                                     y += 35
                                     for (let h = 0; h < rows.length; h++) {
                                         let cat = rows[h]
-                                        doc
-                                            .font('Helvetica')
-                                            .fontSize(18)
-                                            .fill('#fbac38')
-                                            .text(cat.var_name, 50, y, { align: 'center' });
+                                        gen.newCat(y,doc,cat.var_name)//Titulo de la categoria
                                         y += 30
                                         var e = 0
                                         for (let j = 0; j < rows1.length; j++) {
                                             if (rows1[j].fk_id_product_category==cat.fk_id_product_category) {
                                                 if (e>6) break
-                                                gen.products(rows1[j],y,doc)
-                                                /*doc.image(path.join(__dirname,'../dbimagesProducts/'+rows1[j].var_name) ,50,y,{
-                                                    fit: [80, 80], align: 'center', valign: 'center'
-                                                  })
-                                                  .font('Times-Roman')
-                                                  .fontSize(15).fillColor('black').text(rows1[j].var_name_product,140,y+10)
-                                                  .fontSize(10).text(rows1[j].text_description)
-                                                  .font('Times-Bold').fontSize(13).fillColor('red').text(rows1[j].dou_price,{align: 'right'})
-                                                */
+                                                gen.products(rows1[j],y,doc)//genera la targeta del producto
                                                 e++
                                                 y += 90
                                             }
