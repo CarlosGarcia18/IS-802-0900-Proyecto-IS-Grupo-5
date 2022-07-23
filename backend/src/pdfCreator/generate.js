de1c072faf13347839a32bce383bdf6facdcb06a
Luis Castillo <luigiernest999@gmail.com>
2022-07-22 22:34:59 -0600

    .

--- backend/src/pdfCreator/generate.js
+++ backend/src/pdfCreator/generate.js
@@ -17,9 +17,9 @@ function create1() {
                     else {
                         if (rows.length > 0) {
                             conection.query(SQLconsult.sqlGetProducts, (err1, rows1, fields) => {
-                                if (err1){
-                                    console.log({ status: '2', msf: err1.sqlMessage }); 
-                                } 
+                                if (err1) {
+                                    console.log({ status: '2', msf: err1.sqlMessage });
+                                }
                                 else {
                                     let doc = new PDFDocument({
                                         pdfVersion: '1.5',
@@ -29,34 +29,34 @@ function create1() {
                                     });
                                     let p = './src/documentPDF/' + rows2[i].id_user + '_suscription.pdf'
                                     doc.pipe(fs.createWriteStream(p));
-        
+
                                     gen.information(doc)//genera la informacion
                                     gen.fonts(doc)//genera las fuentes
                                     var y = 60
-                                    gen.head(y,doc)//Creacion del encabezado
+                                    gen.head(y, doc)//Creacion del encabezado
                                     y += 35
                                     for (let h = 0; h < rows.length; h++) {
                                         let cat = rows[h]
-                                        gen.newCat(y,doc,cat.var_name)//Titulo de la categoria
+                                        gen.newCat(y, doc, cat.var_name)//Titulo de la categoria
                                         y += 30
                                         var e = 0
                                         for (let j = 0; j < rows1.length; j++) {
-                                            if (rows1[j].fk_id_product_category==cat.fk_id_product_category) {
-                                                if (e>6) break
-                                                gen.products(rows1[j],y,doc)//genera la targeta del producto
+                                            if (rows1[j].fk_id_product_category == cat.fk_id_product_category) {
+                                                if (e > 6) break
+                                                gen.products(rows1[j], y, doc)//genera la targeta del producto
                                                 e++
                                                 y += 90
                                             }
                                         }
-                                        y=60
-                                        if(h!=(rows.length-1)){
+                                        y = 60
+                                        if (h != (rows.length - 1)) {
                                             doc.addPage()
                                         }
                                     }
                                     doc.end()
                                 }
                             })
-                            
+
                         }
                     }
                 })
