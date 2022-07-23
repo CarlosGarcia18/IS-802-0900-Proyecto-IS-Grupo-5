e2ad094292ab6c9649d2a0d836a5f5e0d6dc8450
Luis Castillo <luigiernest999@gmail.com>
2022-07-23 00:12:21 -0600

    Arreglo bug de pagina en blanco

--- backend/src/pdfCreator/SQLconsults.js
+++ backend/src/pdfCreator/SQLconsults.js
@@ -7,11 +7,14 @@ function sqlSuscriptions(id) {
     return sql
 }
 
-var sqlGetProducts = "SELECT fk_id_product_category,product.var_name AS var_name_product,text_description,"
+function sqlGetProducts(id){
+    sql = "SELECT fk_id_product_category,product.var_name AS var_name_product,text_description,"
     + "dou_price,photographs.var_name FROM product LEFT OUTER JOIN  photographs ON "
-    + "photographs.fk_id_product=product.id_product WHERE "
+    + `photographs.fk_id_product=product.id_product WHERE fk_id_department = ${id} AND `
     + `bit_availability = 1 GROUP BY product.id_product `
     + "ORDER BY int_views DESC"
+    return sql
+} 
 
 
 module.exports =
--- backend/src/pdfCreator/generate.js
+++ backend/src/pdfCreator/generate.js
@@ -4,7 +4,7 @@ var PDFDocument = require('pdfkit');
 var fs = require('fs');
 const path = require('path')
 const SQLconsult = require('./SQLconsults')
-
+create()
 function create() {
     conection.query(SQLconsult.sqlGetUsers, (err2, rows2, fields) => {
         if (err2) console.log({ status: '0', msf: err.sqlMessage });
@@ -14,7 +14,7 @@ function create() {
                     if (err) console.log({ status: '1', msf: err.sqlMessage });
                     else {
                         if (rows.length > 0) {
-                            conection.query(SQLconsult.sqlGetProducts, (err1, rows1, fields) => {
+                            conection.query(SQLconsult.sqlGetProducts(rows2[i].fk_id_department), (err1, rows1, fields) => {
                                 if (err1) {
                                     console.log({ status: '2', msf: err1.sqlMessage });
                                 }
@@ -33,23 +33,35 @@ function create() {
                                     var y = 60
                                     gen.head(y, doc)//Creacion del encabezado
                                     y += 35
+                                    var addNewPage = false
                                     for (let h = 0; h < rows.length; h++) {
                                         let cat = rows[h]
-                                        gen.newCat(y, doc, cat.var_name)//Titulo de la categoria
-                                        y += 30
-                                        var e = 0
+                                        let emptyProducts = true //verificacion si no hay productos en esa categoria
                                         for (let j = 0; j < rows1.length; j++) {
                                             if (rows1[j].fk_id_product_category == cat.fk_id_product_category) {
-                                                if (e > 6) break
-                                                gen.products(rows1[j], y, doc, e)//genera la targeta del producto
-                                                e++
-                                                y += 90
+                                               emptyProducts = false
+                                               break
                                             }
                                         }
-                                        y = 60
-                                        if (h != (rows.length - 1)) {
-                                            doc.addPage()
+                                        if(!emptyProducts){
+                                            if (h != (rows.length - 1) && addNewPage) doc.addPage()
+                                            addNewPage = true    
+                                            
+                                            gen.newCat(y, doc, cat.var_name)//Titulo de la categoria
+                                            y += 30
+                                            var e = 0
+                                            for (let j = 0; j < rows1.length; j++) {
+                                                if (rows1[j].fk_id_product_category == cat.fk_id_product_category) {
+                                                    if (e > 6) break
+                                                    gen.products(rows1[j], y, doc, e)//genera la targeta del producto
+                                                    e++
+                                                    y += 90
+                                                }
+                                            }
+                                            y = 60
+                                            
                                         }
+                                        
                                     }
                                     doc.end()
                                 }
